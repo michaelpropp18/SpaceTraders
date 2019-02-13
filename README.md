8a072530835f958aef1c6253121421045c55a425
Matthew Brown <matthewbrown@gatech.edu>
2019-02-13 15:13:55 -0500

    Initial commit of existing sources

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the remote repository for our CS 2340 project, Space Trader.
---
+++ app/src/main/java/com/example/spacetrader/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.spacetrader;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+	private TextView mTextMessage;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mTextMessage = (TextView) findViewById(R.id.message);
+	}
+
+}
